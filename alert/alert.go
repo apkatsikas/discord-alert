365487e812cfccbfd147969752a6b40e46cf39b8
apkatsikas <apkatsikas@gmail.com>
2025-09-13 11:02:49 -0400

    Add package

---
+++ alert/alert.go
@@ -0,0 +1,48 @@
+package alert
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/diamondburned/arikawa/v3/session"
+)
+
+func SendAlert(alertContent string) error {
+	if alertContent == "" {
+		return fmt.Errorf("alert cannot be empty")
+	}
+	var token = os.Getenv("BOT_TOKEN")
+	if token == "" {
+		return fmt.Errorf("no $BOT_TOKEN given")
+	}
+
+	var channelString = os.Getenv("CHANNEL_ID")
+	if channelString == "" {
+		return fmt.Errorf("no $CHANNEL_ID given")
+	}
+
+	s := session.New("Bot " + token)
+
+	channelID, err := discord.ParseSnowflake(channelString)
+
+	if err != nil {
+		return fmt.Errorf("failed to parse snowflake from string %v", channelString)
+	}
+
+	s.AddIntents(gateway.IntentGuildMessages)
+
+	if err := s.Open(context.Background()); err != nil {
+		return fmt.Errorf("failed to connect %v", err)
+	}
+	defer s.Close()
+
+	msg, err := s.SendMessage(discord.ChannelID(channelID), alertContent)
+
+	if err != nil {
+		return fmt.Errorf("failed to send message %v - error was %v", msg, err)
+	}
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,45 +2,9 @@
 // all messages it sees into stderr.
 package main
 
-import (
-	"context"
-	"log"
-	"os"
-
-	"github.com/diamondburned/arikawa/v3/discord"
-	"github.com/diamondburned/arikawa/v3/gateway"
-	"github.com/diamondburned/arikawa/v3/session"
-)
+import "github.com/apkatsikas/discord-alert/alert"
 
 func main() {
-	var token = os.Getenv("BOT_TOKEN")
-	if token == "" {
-		log.Fatalln("No $BOT_TOKEN given.")
-	}
-
-	var channelString = os.Getenv("CHANNEL_ID")
-	if channelString == "" {
-		log.Fatalln("No $CHANNEL_ID given")
-	}
-
-	s := session.New("Bot " + token)
-
-	channelID, err := discord.ParseSnowflake(channelString)
-
-	if err != nil {
-		log.Fatalf("failed to parse snowflake from string %v", channelString)
-	}
-
-	s.AddIntents(gateway.IntentGuildMessages)
-
-	if err := s.Open(context.Background()); err != nil {
-		log.Fatalln("Failed to connect:", err)
-	}
-	defer s.Close()
-
-	msg, err := s.SendMessage(discord.ChannelID(channelID), "ALERT!")
-
-	if err != nil {
-		log.Fatalf("failed to send message %v - error was %v", msg, err)
-	}
+	// TODO - remove, add README, try using it in app
+	alert.SendAlert("Alert content!")
 }
